10d198a23cd4907f1f0790db86f7ff9ea82d5146
Stig Henriksen <mail@stighenriksen.com>
2015-08-23 19:23:02 +0200

    Giving up on flask_migrate, tests will not pass.

--- default.nix
+++ default.nix
@@ -7,18 +7,9 @@
       url = "https://pypi.python.org/packages/source/F/Flask-SQLAlchemy/${name}.tar.gz";
       md5 = "dca034849ffcbaef7b848e8728d3668f";
     };
-        buildInputs = with pkgs; [ python34Packages.flask python34Packages.sqlalchemy_1_0 ];
+        buildInputs = with pkgs; [ pkgs.which python34Packages.flask python34Packages.sqlalchemy_1_0 ];
     };
 
-    flask_migrate = pkgs.python34Packages.buildPythonPackage rec {
-    name = "Flask-Migrate-1.5.0";
-    src = pkgs.fetchurl {
-        url = "https://pypi.python.org/packages/source/F/Flask-Migrate/${name}.tar.gz";
-        md5 = "a9bbc66cbcb3913d1de43f69d8b35631";
-    };
-            buildInputs = with pkgs; [ python34Packages.flask python34Packages.sqlalchemy_1_0 flask_script python34Packages.pytest python34Packages.alembic flask_sqlalchemy
-            python34Packages.python];
-    };
 
    flask_script = pkgs.python34Packages.buildPythonPackage rec {
     name = "Flask-Script-2.0.5";
@@ -44,7 +35,7 @@
     name = "trackmylifts";
     version = "0.1.0.0";
     src = ./.;
-    buildInputs = [ python flask emacs gunicorn sqlalchemy flask_sqlalchemy psycopg2 postgresql zsh flask_migrate];
+    buildInputs = [ python flask emacs gunicorn sqlalchemy flask_sqlalchemy psycopg2 postgresql zsh];
     APP_SETTINGS="config.DevelopmentConfig";
     DATABASE_URL="postgresql://localhost/trackmylifts";
   }
