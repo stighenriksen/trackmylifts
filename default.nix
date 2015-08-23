7a505f60cbcf0d039f1a697e29a8dd4d23d15619
Stig Henriksen <mail@stighenriksen.com>
2015-08-23 12:51:29 +0200

    Preparation for heroku

--- default.nix
+++ default.nix
@@ -1,11 +1,13 @@
   let
     pkgs = import <nixpkgs> {};
   in
-  { stdenv ? pkgs.stdenv, python ? pkgs.python3, flask ? pkgs.python34Packages.flask }:
+  { stdenv ? pkgs.stdenv, python ? pkgs.python3, flask ? pkgs.python34Packages.flask, emacs ? pkgs.emacs , psycopg2 ? pkgs.python34Packages.psycopg2,
+    gunicorn ? pkgs.python34Packages.gunicorn
+ }:
   
   stdenv.mkDerivation {
     name = "trackmylifts";
     version = "0.1.0.0";
     src = ./.;
-    buildInputs = [ python flask ];
+    buildInputs = [ python flask emacs];
   }
