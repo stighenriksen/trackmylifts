c86a4c1eba751abab279d7f6a8a65a05daebe366
Stig Henriksen <mail@stighenriksen.com>
2015-08-23 13:12:12 +0200

    Add config file.

--- default.nix
+++ default.nix
@@ -3,11 +3,12 @@
   in
   { stdenv ? pkgs.stdenv, python ? pkgs.python3, flask ? pkgs.python34Packages.flask, emacs ? pkgs.emacs , psycopg2 ? pkgs.python34Packages.psycopg2,
     gunicorn ? pkgs.python34Packages.gunicorn
- }:
+  }:
   
   stdenv.mkDerivation {
     name = "trackmylifts";
     version = "0.1.0.0";
     src = ./.;
     buildInputs = [ python flask emacs];
+    APP_SETTINGS="config.DevelopmentConfig";
   }
